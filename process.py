e7f297263fad5bde43f3d54de55fcaf4f9544a9e
Steve Armstrong <steve@horsefire.com>
2019-01-02 10:54:17 -0500

    Day 1 second part

--- process.py
+++ process.py
@@ -1,6 +1,11 @@
 #!/usr/local/bin/python
 frequency = 0
-with open("input.txt", "r") as inputFile:
-    for line in inputFile:
-        frequency += int(line)
-print frequency
+seenFrequencies = set()
+while True:
+    with open("input.txt", "r") as inputFile:
+        for line in inputFile:
+            frequency += int(line)
+            if frequency in seenFrequencies:
+                print frequency
+                exit(0)
+            seenFrequencies.add(frequency)
