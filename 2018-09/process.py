14ba9156166367654f6de7bbf0ce5b758e3cf157
Steve Armstrong <steve@horsefire.com>
2019-04-14 20:43:13 -0700

    Got harness in place for speed improvement

--- 2018-09/process.py
+++ 2018-09/process.py
@@ -1,15 +1,7 @@
 #!/usr/bin/env python
 
 import re
-
-with open("input.txt", "r") as inputFile:
-    regex = re.compile("([0-9]+) players; last marble is worth ([0-9]+) points")
-    for line in inputFile:
-        result = regex.match(line)
-        if not result:
-            raise Exception("Something wrong with input: {}. Got result {}".format(line, result))
-        players = int(result.group(1))
-        lastMarble = int(result.group(2))
+import time
 
 def takeTurn(field, currentMarbleIndex, newMarbleValue):
     if newMarbleValue % 23 == 0:
@@ -25,13 +17,48 @@ def takeTurn(field, currentMarbleIndex, newMarbleValue):
 
     return newField, insertIndex, 0
 
-def printHighestScore(players, lastMarble):
+def getHighestScore(players, lastMarble):
+    playerScores = [0 for i in range(players)]
+    currentPlayer = 0
+
     field = [0]
     currentMarbleIndex = 0
     marbleValue = 1
-    while marbleValue <= 25:
+    while marbleValue <= lastMarble:
         field, currentMarbleIndex, capturedValue = takeTurn(field, currentMarbleIndex, marbleValue)
         marbleValue += 1
-        print(field)
 
-printHighestScore(players, lastMarble)
+        playerScores[currentPlayer] += capturedValue
+        currentPlayer = (currentPlayer + 1) % players
+    return max(playerScores)
+
+def printHighestScore(players, lastMarble):
+    highestScore = getHighestScore(players, lastMarble)
+    print("High score for {} players after {} marble: {}".format(players, lastMarble, highestScore))
+    return highestScore
+
+# Part 1
+# printHighestScore(462, 71938) # 398371
+
+# Part 2
+# printHighestScore(462, 7193800)
+
+def speedTest(players, lastMarble):
+    start = time.time()
+    highestScore = getHighestScore(players, lastMarble)
+    end = time.time()
+    print("For {} took {}s".format(lastMarble, end - start))
+
+speedTest(30, 15000)
+
+def checkLogic():
+    knownGoodResults = [(10, 1618, 8317),
+                        (13, 7999, 146373),
+                        (17, 1104, 2764),
+                        (21, 6111, 54718),
+                        (30, 5807, 37305)]
+    for players, lastMarble, highestScoreExpected in knownGoodResults:
+        highestScore = getHighestScore(players, lastMarble)
+        if highestScore != highestScoreExpected:
+            print("Failed. For {}/{} got {} but expected {}".format(players, lastMarble, highestScore, highestScoreExpected))
+checkLogic()
