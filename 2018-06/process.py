e2ff7e1a10ad2ff33c0c3d1b7048e80ba5eba1b3
Steve Armstrong <steve@horsefire.com>
2019-04-14 17:56:35 -0700

    Day 6 complete

--- 2018-06/process.py
+++ 2018-06/process.py
@@ -1,6 +1,8 @@
 #!/usr/bin/env python
 
-from collections import defaultdict
+from collections import defaultdict, Counter
+from functools import reduce
+from itertools import chain
 import re
 
 # Read the points
@@ -30,10 +32,7 @@ def fieldSize(points):
         maxY = max(maxY, point[1])
     return (minX, minY, maxX, maxY)
 
-def createField(points):
-    # None unresolved
-    # -1 tie
-    # [0-9]+ closest to that point
+def createField(points, pointCreator):
     minX, minY, maxX, maxY = fieldSize(points)
     field = []
     for x in range(maxX - minX):
@@ -41,46 +40,60 @@ def createField(points):
         field.append(column)
         for y in range(maxY - minY):
             currentLocation = (x + minX, y + minY)
-            closest = None
-            for pointIndex in range(len(points)):
-                pointDistance = distance(currentLocation, points[pointIndex])
-                if closest is None or pointDistance < closest[1]:
-                    closest = (pointIndex, pointDistance)
-                elif pointDistance == closest[1]:
-                    closest = (-1, pointDistance)
-            column.append(closest)
+            point = pointCreator(currentLocation)
+            column.append(point)
     return field
 
 def edges(field):
-    edgeNumbers = set()
     top = [column[0] for column in field]
     bottom = [column[-1] for column in field]
-    left = field[0]
-    right = field[-1]
-    for location in top + bottom + left + right:
-        edgeNumbers.add(location[0])
-    return edgeNumbers
+    left = field[0][1:-2]
+    right = field[-1][1:-2]
+    return top + bottom + left + right
 
-def frequency(field):
-    freq = defaultdict(int)
-    for column in field:
-        for location in column:
-            freq[str(location[0])] += 1
-    return freq
+def printLargestFiniteField(points):
+    # None unresolved
+    # -1 tie
+    # [0-9]+ closest to that point
+    def closestPoint(currentLocation):
+        closest = None
+        for pointIndex in range(len(points)):
+            pointDistance = distance(currentLocation, points[pointIndex])
+            if closest is None or pointDistance < closest[1]:
+                closest = (pointIndex, pointDistance)
+            elif pointDistance == closest[1]:
+                closest = (-1, pointDistance)
+        return closest
 
-field = createField(points)
-infinite = edges(field)
-freq = frequency(field)
+    field = createField(points, closestPoint)
+    infinite = set([location[0] for location in edges(field)])
+    frequency = Counter(map(lambda location: location[0], chain(*field)))
 
-largestField = None
-for fieldName, fieldSize in freq.iteritems():
-    fieldIndex = int(fieldName)
-    if fieldIndex == -1 or fieldIndex in infinite:
-        continue
-    if largestField is None or fieldSize > largestField[1]:
-        largestField = (fieldIndex, fieldSize)
+    largestField = None
+    for fieldName, fieldSize in frequency.iteritems():
+        fieldIndex = int(fieldName)
+        if fieldIndex == -1 or fieldIndex in infinite:
+            continue
+        if largestField is None or fieldSize > largestField[1]:
+            largestField = (fieldIndex, fieldSize)
 
-print("Have {} points".format(len(points)))
-print("{} infinite fields".format(len(infinite)))
-print("Frequencies: {}".format(freq))
-print("Largest field {} is size {}".format(*largestField))
+    print("Have {} points".format(len(points)))
+    print("Dropping {} infinite fields".format(len(infinite)))
+    print("Largest field {} is size {}".format(*largestField)) # Largest field 4 is size 2906
+
+printLargestFiniteField(points)
+
+def printLargestTotalDistanceField(points):
+
+    def totalDistancePoint(currentLocation):
+        distanceToAllPoints = 0
+        for point in points:
+            distanceToAllPoints += distance(currentLocation, point)
+        return distanceToAllPoints
+
+    field = createField(points, totalDistancePoint)
+    maxDistance = 10000
+    locationsLowEnough = [x for x in chain(*field) if x <= maxDistance]
+    print("{} locations had a total distance <= {}".format(len(locationsLowEnough), maxDistance))
+
+printLargestTotalDistanceField(points) # 50530 locations had a total distance <= 10000
